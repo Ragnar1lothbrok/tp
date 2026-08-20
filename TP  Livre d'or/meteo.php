baae30895b3896b05e166ea7eb4a2237b847d833
Ragnar1lothbrok <mohamedkhsib24@gmail.com>
2026-08-20 15:13:35 +0100

    hide my api

--- TP  Livre d'or/meteo.php
+++ TP  Livre d'or/meteo.php
@@ -8,7 +8,7 @@ $today = null;
 $forecast = null;
 
 try {
-    $weather = new OpenWeather('fdb93d06e0e2512635156e672544c3ee');
+    $weather = new OpenWeather('.');
     $today = $weather->getToday('Tunis');
     $forecast = $weather->getForecast('Tunis');
 } catch (Exception | Error $e) {

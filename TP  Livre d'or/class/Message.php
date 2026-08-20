ccfb759d4b83af82b3d3ab24204dda69836f5eb7
Ragnar1lothbrok <mohamedkhsib24@gmail.com>
2026-08-20 11:49:29 +0100

    add tp Livre d'or

---
+++ TP  Livre d'or/class/Message.php
@@ -0,0 +1,59 @@
+<?php
+class Message {
+
+    const LIMIT_USERNAME = 3;
+    const LIMIT_MESSAGE = 10;
+
+    private $username;
+    private $message;
+    private $date;
+
+    public function __construct(string $username, string $message, ?DateTime $date = null)
+    {
+        $this->username = $username;
+        $this->message = $message;
+        $this->date = $date ?? new DateTime();
+    }
+
+    public function isValid(): bool
+    {
+        return empty($this->getErrors());
+    }
+
+    public function getErrors(): array
+    {
+        $errors = [];
+        if (mb_strlen($this->username) < self::LIMIT_USERNAME) {
+            $errors['username'] = "Votre pseudo est trop court";
+        }
+        if (mb_strlen($this->message) < self::LIMIT_MESSAGE) {
+            $errors['message'] = "Votre message est trop court";
+        }
+        return $errors;
+    }
+
+    public function toHTML(): string
+    {
+        $username = htmlentities($this->username);
+        $this->date->setTimezone(new DateTimeZone('Africa/Tunis'));
+        $date = $this->date->format('d/m/Y à H:i');
+        $message = nl2br(htmlentities($this->message));
+
+        return "<p><strong>{$username}</strong> <em>le {$date}</em><br>{$message}</p>";
+    }
+
+    public function toJSON(): string
+    {
+        return json_encode([
+            'username' => $this->username,
+            'message'  => $this->message,
+            'date'     => $this->date->getTimestamp()
+        ]);
+    }
+
+    public static function fromJSON(string $json): Message
+    {
+        $data = json_decode($json, true);
+        return new self($data['username'], $data['message'], new DateTime("@" . $data['date']));
+    }
+}
